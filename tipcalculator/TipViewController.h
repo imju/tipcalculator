fdbe98f5180865634d63977b8c59033e3a370ac1
ibyon <imjubyon@gmail.com>
2013-09-18 11:39:59 -0700

    checking tip calculator code

---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by bgbb on 9/17/13.
+//  Copyright (c) 2013 Qpoo Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
